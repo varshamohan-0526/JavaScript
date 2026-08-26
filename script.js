42df6125ef25a297fbf143d9958a19197be3308d
varshamohan-0526 <varshamohan0526@gmail.com>
2026-08-26 10:40:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// 1
+let s_name="varsha";
+let s_age=22;
+let s_mark=99;
+console.log(s_name);
+console.log(s_age);
+console.log(s_mark);
+
+
+//2
+let u_name=prompt("Enter first name");
+alert(u_name);
+
+
+//3
+let u_age=prompt("Enter age");
+console.log(u_age);
+
+
+//4
+var mark=confirm("Are u eligible to vote??");
+console.log(mark);
+
+
+//5
+let user_name=prompt("Enter second name");
+document.writeln(user_name);
+
+
+
